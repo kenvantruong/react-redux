1f9d0c2171f688f6b8796e2360b6cfe7a6a5d28e
Ken Truong <kenvantruong@gmail.com>
2018-08-05 22:53:13 -0700

    first commit

---
+++ assets/js/firstComp/firstComp.js
@@ -0,0 +1,21 @@
+import React, { Component} from 'react'
+import ReactDOM from 'react-dom'
+
+class Layout extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Joe'
+    }
+  }
+  clickedBtn = () => {
+    console.log('swag')
+  }
+  render () {
+    return ()
+  }
+}
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<Layout />, app)
---
+++ public/js/components/firstComp.js
@@ -0,0 +1,10 @@
+webpackJsonp([1],{
+
+/***/ 98:
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: SyntaxError: Unexpected token (15:12)\n\n  13 |   }\n  14 |   render () {\n> 15 |     return ()\n     |             ^\n  16 |   }\n  17 | }\n  18 | \n");
+
+/***/ })
+
+},[98]);
